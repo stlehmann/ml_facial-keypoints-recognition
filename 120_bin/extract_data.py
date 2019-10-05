159339b844b3ef2aef4b1695529871ad34110c8f
Stefan Lehmann <stlm@posteo.de>
2019-10-05 13:33:47 +0000

    Make extract_data py35 compatible

--- 120_bin/extract_data.py
+++ 120_bin/extract_data.py
@@ -5,13 +5,13 @@ from zipfile import ZipFile, ZipInfo
 from pyprojroot import here
 
 
-data_p: pathlib.Path = here("020_data")
+data_p = here("020_data")
 assert data_p.is_dir()
 
-archive = ZipFile(data_p / "facial-keypoints-detection.zip")
-archive.extractall(data_p)
+archive = ZipFile(str(data_p / "facial-keypoints-detection.zip"))
+archive.extractall(str(data_p))
 
 for fn in ("test.zip", "training.zip"):
-    archive = ZipFile(data_p / fn)
-    archive.extractall(data_p)
+    archive = ZipFile(str(data_p / fn))
+    archive.extractall(str(data_p))
     (data_p / fn).unlink()
